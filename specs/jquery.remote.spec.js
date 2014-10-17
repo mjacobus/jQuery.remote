68a39a9f430685dd1c0f141cf0a332249a20323c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-10-17 09:53:33 -0300

    Little refactory

--- specs/jquery.remote.spec.js
+++ specs/jquery.remote.spec.js
@@ -1,11 +1,9 @@
 describe("jQuery.remote()", function () {
-  var $container, $anchor;
-
   it("returns the selected element", function () {
     loadFixtures('example_link.html');
-    $anchor = $('a[data-remote]');
-    expect($anchor.remote()).toBe($anchor);
-    expect($anchor.remote().length).toEqual(2);
+    anchor = $('a[data-remote]');
+    expect(anchor.remote()).toBe(anchor);
+    expect(anchor.remote().length).toEqual(2);
   });
 });
 
